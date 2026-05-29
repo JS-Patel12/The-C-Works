53ee7c6c9d71ede016ad4d3ecdfd85dd25d690df
JS-Patel12 <jaypatel122010@gmail.com>
2026-05-29 21:26:23 +0530

    The-C-Works

--- Ch-9 Pattern/Custom Pattern/Pattern 5.c
+++ Ch-9 Pattern/Custom Pattern/Pattern 5.c
@@ -5,16 +5,19 @@ main()
     int i,j,s;
     
     for(i=1;i<=5;i++)
-	{   
-        for(s=1;s<i;s++)
+	{
+        for(s=1;s<=5-i;s++)
 		{
             printf(" ");
         }
-        for(j=5;j>=i;j--)
+        for(j=1;j<=i;j++)
 		{
-            printf("%d",j);
+            printf("*");
         }
-        
+        for(j=i-1;j>=1;j--)
+		{
+            printf("*");
+        }
         printf("\n");
     }
 }
--- Ch-9 Pattern/With Space/Pattern 5.c
+++ Ch-9 Pattern/With Space/Pattern 5.c
@@ -4,13 +4,13 @@ main()
 {
     int i,j,s;
     
-    for(i=1;i<=5;i++)
+    for(i=5;i>=1;i--)
 	{   
-        for(s=1;s<i;s++)
+        for(s=i;s<5;s++)
 		{
             printf(" ");
         }
-        for(j=5;j>=i;j--)
+        for(j=i;j>=1;j--)
 		{
             printf("%d",j);
         }
